cce2036fafbc4056e7dc8d9d7ea98663f26ca28a
MKarimi <mkarimi21@hotmail.com>
2019-11-28 09:10:29 +0330

    example 01

---
+++ Starter/Examples/01_Sequence_Operator.py
@@ -0,0 +1,18 @@
+``` python3
+# Sequence and Operators in Python v3
+
+a = [26, "alireza", "Abolfazl", 526.25, -3, 6]
+
+pi = 3.141596
+r = input("Enter Ur radus= ")
+s = (float(r)**2)*pi
+m = (float(r)*2)*pi
+
+A = input("Enter ur guess for ")
+
+
+b = int(a[0])+float(a[3]) 
+c = str(a[0]) + a[-4] 
+
+print ("Ur len= ", len(a), '|', s, '|', m)
+print ()
